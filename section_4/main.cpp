f6106d30f53313549527812ff46864a5f0f9ebca
gota morishita <optimistic.gota@gmail.com>
2021-06-26 17:40:50 +0900

    first commit

---
+++ section_4/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  int favorite_num;
+  std::cout << "Enter your favorite number between 1 and 100: ";
+  std::cin >> favorite_num;
+  std::cout << "Amazing!! That's my favorite number, too!" << std::endl;
+  return 0;
+}
